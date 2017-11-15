77edbc56b0b7bc3fcade1a47c895c8f9a8eea851
erodriguezfeliz <erodriguezfeliz@gmail.com>
2017-11-15 01:25:19 -0400

    adding component and preprocessors

--- dataVis/mark_down/src/App.js
+++ dataVis/mark_down/src/App.js
@@ -1,21 +1,63 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import showdown from 'showdown';
 import logo from './logo.svg';
 import './App.css';
+import { textMarkdown } from './textExample';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      text: textMarkdown
+    };
+
   }
+  markerToText = (text) => {
+    let converter = new showdown.Converter();
+    return converter.makeHtml(text);
+  }
+
+  onChangeEdior = (e) => {
+    let text = e.target.value;
+    this.setState({text: text});
+  }
+
+  render() {
+    return(
+    <div className="flex-row container">
+        <Editor
+          onChangeEdior={this.onChangeEdior}
+          text={this.state.text}
+          />
+
+        <ScreenDisplay
+          text={this.state.text}
+          markerToText={this.markerToText}
+          />
+     </div>
+    )
+  }
+}
+
+const Editor = (props) => (
+    <textarea type="text" rows="35" onChange={(e)=> props.onChangeEdior(e)}>
+      {props.text}
+     </textarea>
+)
+
+const ScreenDisplay = (props) => (
+  <div>
+    <span dangerouslySetInnerHTML={{__html: props.markerToText(props.text)}}></span>
+  </div>
+)
+
+Editor.propTypes = {
+  text: PropTypes.string.isRequired
+}
+
+ScreenDisplay.propTypes = {
+  text: PropTypes.string.isRequired
 }
 
 export default App;
---
+++ dataVis/mark_down/src/textExample.js
@@ -0,0 +1,22 @@
+export const textMarkdown = `Heading
+=======
+
+Sub-heading
+-----------
+
+### Another deeper heading
+
+Paragraphs are separated
+by a blank line.
+
+Leave 2 spaces at the end of a line to do a
+line break
+
+Text attributes *italic*, **bold**,
+monospace, ~~strikethrough~~ .
+
+Shopping list:
+
+* apples
+* oranges
+* pears`;
