17fa79272a5217d0001bbedf2fb78829cc986c02
angel.rodriguez <angel.rodriguez@nagico.com>
2017-11-16 12:45:44 -0400

    all done

--- dataVis/camper_leaderboard/src/Header.js
+++ dataVis/camper_leaderboard/src/Header.js
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types';
 
  const Header = (props) => (
   <div className="flex-row header">
-    <button onClick={() => props.onClickSortByRecent()}>Sort by last 30 days</button>
-    <button onClick={() => props.onClickSortByAllTime()}>Sort by all time</button>
+    <button onClick={() => props.onClickSortByRecent("allTime", "alltime")}>Sort by last 30 days</button>
+    <button onClick={() => props.onClickSortByAllTime("recent","recent")}>Sort by all time</button>
   </div>
 )
 
