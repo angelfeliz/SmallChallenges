dde46f8f464ad2fd558b7f9facb9082ea9d30256
angelfeliz <erodriguezfeliz@gmail.com>
2017-11-24 07:35:08 -0400

    working on board

--- dataVis/game_of_life/src/App.js
+++ dataVis/game_of_life/src/App.js
@@ -11,20 +11,31 @@ class App extends Component {
       <div className="App">
         <div className="board-game">
         <p>hello</p>
-          <div className="column">
-            <div className="cell"></div>
-            <div className="cell"></div>
-            <div className="cell"></div>
-          </div>
-          <div className="column">
-            <div className="cell"></div>
-            <div className="cell"></div>
-            <div className="cell"></div>
-          </div>
+    {board()}
         </div>
       </div>
     );
   }
+}
+
+const board = () => {
+  let boardPaint
+  let cell = [];
+  let top = [];
+  for(var x = 0; x < 5; x++)
+  {
+
+    for(var y = 0; y < 5; y++)
+    {
+      cell.push(<div className="cell"></div>)
+    }
+    top.push(<div className="column"></div>)
+
+
+    boardPaint = top.concat(cell);
+  }
+  console.log(boardPaint);
+  return boardPaint;
 }
 
 export default App;
