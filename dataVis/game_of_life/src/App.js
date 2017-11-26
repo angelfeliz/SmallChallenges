93705f704e17392937ce6c3820e29d6c537f5ea2
angelfeliz <erodriguezfeliz@gmail.com>
2017-11-26 10:25:37 -0400

    work on tree board

--- dataVis/game_of_life/src/App.js
+++ dataVis/game_of_life/src/App.js
@@ -5,37 +5,63 @@ const COLUMN = 70;
 const ROW = 50;
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    let obj = {};
+    for(let x = 1; x <= 3; x++)
+    {
+      let obje = {
+        [x]:{}
+      }
+      for(let y = 1 ; y <= 3; y++)
+      {
 
+        obje[x][y] =  {
+                     "on": false
+                   }
+      }
+      obj = {...obj, ...obje}
+    }
+    this.state = {board:obj};
+    console.log(this.state)
+  }
+
+
+  nextGeneration = (col, row) => {
+    let element = document.getElementsByName(`${col}-${row}`);
+    console.log(element);
+    element.className = "cell on"
+    React.cloneElement(element);
+  }
   render() {
     return (
       <div className="App">
         <div className="board-game">
-        <p>hello</p>
-    {board()}
+          {Board(5,5)}
         </div>
+        <button onClick={()=>this.nextGeneration(1,1)}>test</button>
       </div>
     );
   }
 }
 
-const board = () => {
-  let boardPaint
-  let cell = [];
-  let top = [];
-  for(var x = 0; x < 5; x++)
-  {
-
-    for(var y = 0; y < 5; y++)
-    {
-      cell.push(<div className="cell"></div>)
-    }
-    top.push(<div className="column"></div>)
+const Board = (col,row) => Column(col,row)
 
 
-    boardPaint = top.concat(cell);
+const Column = (col,row) => {
+  let column = [];
+  for (var x = 0; x < col; x++) {
+    column.push(<div key={x} className="column">{Cell(x,row)}</div>)
   }
-  console.log(boardPaint);
-  return boardPaint;
+  return column;
+}
+
+const Cell = (col,row) => {
+  let cell = [];
+  for (var x = 0; x < row; x++) {
+      cell.push(<div key={x} className="cell" data-col={col} data-cell={x} name={`${col}-${x}`}></div>);
+  }
+  return cell;
 }
 
 export default App;
